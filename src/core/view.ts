a28c4a4893eee4d1dd85f2b3920c13a64350052c
Douglas Orr <douglas.orr99@gmail.com>
2025-11-10 06:13:03 +0000

    Show a texture

--- src/core/view.ts
+++ src/core/view.ts
@@ -8,11 +8,31 @@ export function start(game: G.Game): void {
   );
 }
 
+type Box = { left: number; right: number; bottom: number; top: number };
+
+class Logger {
+  private trigger: boolean = false;
+  constructor() {
+    window.addEventListener("click", (e) => {
+      this.trigger = true;
+    });
+  }
+  log(...args: any[]) {
+    if (this.trigger) {
+      console.log(...args);
+      this.trigger = false;
+    }
+  }
+}
+
 class Renderer {
   private readonly renderer: THREE.WebGLRenderer;
   private readonly scene: THREE.Scene;
   private readonly camera: THREE.OrthographicCamera;
   private lastTime: number | null = null;
+  private readonly logger: Logger = new Logger();
+
+  private gridBg: THREE.Mesh;
 
   constructor(game: G.Game, canvas: HTMLCanvasElement) {
     void game; // TODO
@@ -34,10 +54,20 @@ class Renderer {
     const w = window.innerWidth;
     const h = window.innerHeight;
     const planeGeometry = new THREE.PlaneGeometry(w / 4, h / 4);
-    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.position.set(w / 2, h / 2, 0);
-    this.scene.add(plane);
+    const texture = new THREE.TextureLoader().load("img/cells.png");
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+    texture.offset.set(0, 0.5);
+    texture.repeat.set(1, 0.5);
+    const planeMaterial = new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+    });
+    this.gridBg = new THREE.Mesh(planeGeometry, planeMaterial);
+    this.gridBg.position.set(w / 2, h / 2, 0);
+    this.scene.add(this.gridBg);
   }
 
   private onResize() {
@@ -59,7 +89,47 @@ class Renderer {
     // const dt = (time - this.lastTime) / 1000;
     this.lastTime = time;
     // TODO - update animations
+
+    const layout = this.topLevelLayout();
+    this.logger.log(layout);
+
+    this.gridBg.position.set(
+      (layout.grid.left + layout.grid.right) / 2,
+      (layout.grid.bottom + layout.grid.top) / 2,
+      0
+    );
+    this.gridBg.scale.set(
+      (layout.grid.right - layout.grid.left) /
+        this.gridBg.geometry.parameters.width,
+      (layout.grid.top - layout.grid.bottom) /
+        this.gridBg.geometry.parameters.height,
+      1
+    );
+
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.onAnimate.bind(this));
+  }
+
+  private topLevelLayout(): { grid: Box; panel: Box } {
+    const w = this.camera.right - this.camera.left;
+    const h = this.camera.top - this.camera.bottom;
+    const pad = 0.02 * w;
+    const panelW = 0.3 * w;
+    const gridSize = Math.min(h - 2 * pad, w - panelW - 3 * pad);
+    const y = h / 2 - gridSize / 2;
+    return {
+      grid: {
+        left: pad,
+        right: pad + gridSize,
+        bottom: y,
+        top: y + gridSize,
+      },
+      panel: {
+        left: pad + gridSize + pad,
+        right: pad + gridSize + pad + panelW,
+        bottom: y,
+        top: y + gridSize,
+      },
+    };
   }
 }
